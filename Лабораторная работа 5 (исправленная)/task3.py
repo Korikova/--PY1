32e31cba4931bc75c9ea6f600ec9001ba164178b
Korikova <115543480+Korikova@users.noreply.github.com>
2022-11-13 22:13:15 +0300

    Лабораторная работа 5 (исправленная)

---
+++ Лабораторная работа 5 (исправленная)/task3.py
@@ -0,0 +1,18 @@
+import random
+
+
+def get_unique_list_numbers(min_value, max_value, max_length) -> list[int]:
+    unique_numbers = []
+
+    while len(unique_numbers) < max_length:
+        list_random = random.randint(min_value, max_value)
+        if list_random not in unique_numbers:
+            unique_numbers.append(list_random)
+
+    return unique_numbers
+
+
+length = 15
+list_unique_numbers = get_unique_list_numbers(-10, 10, length)
+print(list_unique_numbers)
+print(len(list_unique_numbers) == len(set(list_unique_numbers)) == length)
